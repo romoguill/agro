29a5a8a3b4854b23851d0efa28c88b7720e29725
Rodrigo Moguillansky <romoguill@gmail.com>
2024-05-03 21:15:18 -0300

    Finished with config service and typeorm module config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.env.production'
+          : process.env.NODE_ENV === 'test'
+            ? '.env.test'
+            : '.env.development',
+      cache: true,
+    }),
+    AuthModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
